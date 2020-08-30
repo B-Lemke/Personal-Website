0374b5616ee0cc876567eeafc1f271395779ab48
Broderick Lemke <blemke4@gmail.com>
2020-08-29 21:46:56 -0500

    Under Construction and page layouts added and font-awesome

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// Import utilities
+import './utilities/random.js';
+
+// Import modules
+import {ReplaceCurrentYear} from './misc/replaceCurrentYear.js';
+import {ReplacePlaceholderEmails} from './misc/replacePlaceholderEmails.js';
+
+
+ReplacePlaceholderEmails();
+ReplaceCurrentYear();
