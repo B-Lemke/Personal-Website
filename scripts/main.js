23c386e13e4b64e2661db59d2de00fe1f3beec48
Broderick Lemke <blemke4@gmail.com>
2020-10-16 15:22:33 -0500

    Blog page, titles, colors, blockquotes, and more!

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,12 @@ import './utilities/random.js';
 // Import modules
 import {ReplaceCurrentYear} from './misc/replaceCurrentYear.js';
 import {ReplacePlaceholderEmails} from './misc/replacePlaceholderEmails.js';
+import {LoadHeadScriptsAndLinks} from './utilities/importFiles.js'
 
+//Load in CSS and JS files
+LoadHeadScriptsAndLinks();
 
+// Footer utilities
 ReplacePlaceholderEmails();
 ReplaceCurrentYear();
+
