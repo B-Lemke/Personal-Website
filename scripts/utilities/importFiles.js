93da00a130217c6c93fa4b1cd17ab122a3baa65c
Broderick Lemke <blemke4@gmail.com>
2020-10-19 09:21:06 -0500

    Fixing style links

--- scripts/utilities/importFiles.js
+++ scripts/utilities/importFiles.js
@@ -1,7 +1,7 @@
 function LoadHeadScriptsAndLinks() {
     document.head.innerHTML += `
-    <link rel="stylesheet" type="text/css" href="/styles/css/style.css">
-    <link href="/assets/font-awesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../../styles/css/style.css">
+    <link href="../../assets/font-awesome/css/all.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
