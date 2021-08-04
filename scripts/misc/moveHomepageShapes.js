7b1aafbadd38a907ef1f27c3431aea8918845550
Broderick Lemke <blemke4@gmail.com>
2021-08-04 08:43:52 -0500

    Space out shapes a bit and added sprite sheet.

--- scripts/misc/moveHomepageShapes.js
+++ scripts/misc/moveHomepageShapes.js
@@ -2,8 +2,6 @@ function MoveShapes() {
   let shapeEls = document.querySelectorAll(".svg-shape");
   var easings = ["linear"];
 
-
-
   // Utility Function to set keyframes on the fly
   function createKeyframes(value) {
     var keyframes = [];
@@ -11,34 +9,60 @@ function MoveShapes() {
     return keyframes;
   }
 
-
   // function to move each shape, recalled as one finishes
   function animateShape(el) {
-
-    var animation = anime.timeline({
-      targets: el,
-      direction: 'alternate',
-      duration: function() { return anime.random(20000, 30000); },
-      easing: function() { return easings[anime.random(0, easings.length - 1)]; },
-      complete: function(anim) { animateShape(anim.animatables[0].target); },
-    })
-    .add({
-      translateX: createKeyframes(function(el) { 
-        return anime.random(-300, 300);
-      }),
-      translateY: createKeyframes(function(el) { 
-        return anime.random(-100, 100);
-      }),
-      rotate: createKeyframes(function() { return anime.random(-180, 180); }),
-    }, 0);
-
+    var animation = anime
+      .timeline({
+        targets: el,
+        direction: "alternate",
+        duration: function () {
+          return anime.random(20000, 30000);
+        },
+        easing: function () {
+          return easings[anime.random(0, easings.length - 1)];
+        },
+        complete: function (anim) {
+          animateShape(anim.animatables[0].target);
+        },
+      })
+      .add(
+        {
+          translateX: createKeyframes(function (el) {
+            return anime.random(-300, 300);
+          }),
+          translateY: createKeyframes(function (el) {
+            return anime.random(-100, 100);
+          }),
+          rotate: createKeyframes(function () {
+            return anime.random(-180, 180);
+          }),
+        },
+        0
+      );
   }
 
-  for (var i = 0; i < shapeEls.length; i++) {
-      
-  // Randomize Shape placement and start the animations
-  shapeEls[i].style.top = anime.random(-10, 90) + '%';
-  shapeEls[i].style.left = anime.random(-10, 80) + '%';
+let lastX = 0;
+
+
+  for (var i = 0; i < shapeEls.length; i++) {    
+    // set random sizes
+    let size = anime.random(100, 500) + "px";
+    shapeEls[i].style.width = size;
+    shapeEls[i].style.height = size;
+
+    // Randomize Shape placement and start the animations
+     // Top has a range between 10 and 95. That's 85 percentages, plus 15 to bring it to the range, even split this way
+    shapeEls[i].style.top = ((85 / shapeEls.length) * i + 15) + '%'; //anime.random(10, 95) + "%";
+
+    let newX = anime.random(-10, 90);
+    while(-20 < newX - lastX && newX - lastX < 20 ) {
+      console.log("Relooping", lastX, newX);
+      newX = anime.random(-10, 90);
+    }
+    lastX = newX;
+    shapeEls[i].style.left =  newX + "%";
+
+
     animateShape(shapeEls[i]);
   }
 }
